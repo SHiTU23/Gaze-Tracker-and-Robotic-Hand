679bdab3a84cde826d6ea49284fcb2046bbfc98c
SHiTU23 <shi.tu2323@gmail.com>
2024-05-28 16:23:39 +0330

    Record data using Netwrok API

---
+++ src/gaze_tracker/pupile_remote.py
@@ -0,0 +1,14 @@
+import zmq
+from time import sleep 
+
+context = zmq.Context()
+pupil_remote = zmq.Socket(context, zmq.REQ)
+### Local Host and Default IP address for Pupil Core Software
+pupil_remote.connect('tcp://127.0.0.1:50020')
+
+pupil_remote.send_string('R')
+print(pupil_remote.recv_string())
+
+sleep(5)
+pupil_remote.send_string('r')
+print(pupil_remote.recv_string())
