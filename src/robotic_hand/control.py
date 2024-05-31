ba422a4c39c1bab00bd0acd953167518a9182646
SHiTU23 <shi.tu2323@gmail.com>
2024-05-31 15:51:12 +0330

    Fix: Error handling the connected port to RH
    + RH : Robotic hand

--- src/robotic_hand/control.py
+++ src/robotic_hand/control.py
@@ -9,14 +9,17 @@ print('available ports:', ports)
 if not ports:
     raise IOError('No port available.')
 
-port = ports[-1] ### COM6 : The port that hand is connetd to
-dxl_io = pypot.dynamixel.DxlIO(port)
-print('Connected!')
+for port in ports:
+    try:
+        dxl_io = pypot.dynamixel.DxlIO(port)
+        print(f'Connected to {port}!')
+        break
+    except:
+        pass
 
-### I know that motor Ids are from 21 to 29
-### So the range for scannign would close to these number 
+
 motor_IDs = dxl_io.scan(range(10, 35))
-del motor_IDs[0] ### motor is 21 is not connected to a motor on hand
+del motor_IDs[0] ### motor ID 21 is not connected to a part in hand
 motor_pose = {}
 for motor in motor_IDs:
     motor_pose[motor] = 0 ### setting the default value for motor positions
@@ -28,12 +31,12 @@ class Motor(Enum):
     thumb = 27
     fingers = 29
 
-# motor_pose[Motor.thumb.value] = 90
+# motor_pose[Motor.fingers.value] = 90
 dxl_io.set_goal_position(motor_pose)
 for motor in motor_IDs:
     print(Motor(motor).name)
     for i in range (-150, 150, 25):
         motor_pose[motor] = i
         dxl_io.set_goal_position(motor_pose)
-        sleep(0.5)
+        sleep(0.1)
 
