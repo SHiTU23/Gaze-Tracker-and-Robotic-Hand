c4713d6bf06669933d7397d8e9fdd05853b86945
SHiTU23 <shi.tu2323@gmail.com>
2024-06-09 12:37:54 +0330

    Fix: move class variables into __init__ func

--- src/robotic_hand/robotic_hand_controlClass.py
+++ src/robotic_hand/robotic_hand_controlClass.py
@@ -9,20 +9,20 @@ class Motor_Name:
     middle_fingers = 'middle_fingers'
 
 class hand_control:
-    ## wrist_R: rotary motor ; wrist_BF: motor for movement in back and forth
-    motor_IDs_list = {Motor_Name.wrist_R  : 0,
-                      Motor_Name.wrist_BF : 0,
-                      Motor_Name.thumb    : 0,
-                      Motor_Name.middle_fingers : 0}
-    _motor_pose = {}
-
-
     def __init__(self, motor_id_scanRange):
+        ###### Defining Variables ######
+        self.motor_IDs_list = { Motor_Name.wrist_R  : 0, ## wrist_R : rotary motor 
+                                Motor_Name.wrist_BF : 0, ## wrist_BF: motor for movement in back and forth
+                                Motor_Name.thumb    : 0,
+                                Motor_Name.middle_fingers : 0}
+        self._motor_pose = {}
+        self._motor_IDs = []
+        self._dxl_io = []
+        ################################
         self._get_motor_IDs(motor_id_scanRange)
 
     def _get_motor_IDs(self, motorID_scanRange):
-        global _motor_IDs, _dxl_io 
-        self._motor_found = False
+        _motor_found = False
 
         _ports = pypot.dynamixel.get_available_ports()
         print('available ports:', _ports)
@@ -32,30 +32,30 @@ class hand_control:
 
         for port in _ports:
             try:
-                _dxl_io = pypot.dynamixel.DxlIO(port)
+                self._dxl_io = pypot.dynamixel.DxlIO(port)
                 try:
-                    _motor_IDs = _dxl_io.scan(range(motorID_scanRange[0], motorID_scanRange[1]))
-                    del _motor_IDs[0] ### motor ID 21 is not connected to a part in hand
-                    for motor in _motor_IDs:
+                    self._motor_IDs = self._dxl_io.scan(range(motorID_scanRange[0], motorID_scanRange[1]))
+                    del self._motor_IDs[0] ### motor ID 21 is not connected to a part in hand
+                    for motor in self._motor_IDs:
                         self._motor_pose[motor] = 0
 
                     for motor_id, motor_name  in enumerate(self.motor_IDs_list):
-                        self.motor_IDs_list[motor_name] = _motor_IDs[motor_id]
+                        self.motor_IDs_list[motor_name] = self._motor_IDs[motor_id]
 
                     
 
                     print(f'Connected to {port}! MotorIDs are: {self.motor_IDs_list}')
-                    self._motor_found = True
+                    _motor_found = True
                     break
                 except:
-                    self._motor_found = False
+                    _motor_found = False
                     pass
             except:
                 ### THE PORT CANNOT GET OPENED
-                self._motor_found = False
+                _motor_found = False
                 pass
         
-        if self._motor_found == False:
+        if _motor_found == False:
             print("XXX NO MOTOR FOUND XXX")
             exit()
         
@@ -65,10 +65,10 @@ class hand_control:
         defualt value is 0
         """
 
-        for motor in _motor_IDs:
+        for motor in self._motor_IDs:
             self._motor_pose[motor] = angle ### setting an angle for motor positions
 
-        _dxl_io.set_goal_position(self._motor_pose)
+        self._dxl_io.set_goal_position(self._motor_pose)
         print("ALL motors Moved to ", angle)
 
     
@@ -79,8 +79,8 @@ class hand_control:
         """
         motor_name.lower()
         self._motor_pose[self.motor_IDs_list[motor_name]] = angle
-        _dxl_io.set_goal_position(self._motor_pose)
-        print( f"motor {motor_name} with ID of {self.motor_IDs_list[motor_name]} Moved to ", angle)
+        self._dxl_io.set_goal_position(self._motor_pose)
+        print( f"motor {motor_name} with ID of {self.motor_IDs_list[motor_name]} Moved to {angle} degree")
 
     def goTo_multiple(self, motor_names_and_angles):
         """
@@ -94,18 +94,33 @@ class hand_control:
             self._motor_pose[motor_name] = angle
 
         ## ALL motors move simulataniously
-        _dxl_io.set_goal_position(self._motor_pose)
+        self._dxl_io.set_goal_position(self._motor_pose)
 
     def goTo_homePose(self):
+        """
+         All motors go to 0 degree
+        """
         self.all_motors_goTo_Pose()
 
-    def open_fingers(self):
-        self._finger_poses = {Motor_Name.thumb : -150,
-                              Motor_Name.middle_fingers : -150}
-        self.goTo_multiple(self._finger_poses)
+    def open_fingers(self, open_angle=-150):
+        """
+         Default value is -150
+         open_angle range: -150 to 150
+         150 is close angle
+        """
+        _finger_poses = {Motor_Name.thumb : open_angle,
+                         Motor_Name.middle_fingers : open_angle}
+        self.goTo_multiple(_finger_poses)
+        print("Hand Opened!")
 
-    def close_fingers(self):
-        self._finger_poses = {Motor_Name.thumb : 150,
-                              Motor_Name.middle_fingers : 150}
-        self.goTo_multiple(self._finger_poses)
+    def close_fingers(self, close_angle=150):
+        """
+         Default value is 150
+         close_angle range: 150 to -150
+         -150 is open angle
+        """
+        _finger_poses = {Motor_Name.thumb : close_angle,
+                         Motor_Name.middle_fingers : close_angle}
+        self.goTo_multiple(_finger_poses)
+        print("Hand Closed!")
 
