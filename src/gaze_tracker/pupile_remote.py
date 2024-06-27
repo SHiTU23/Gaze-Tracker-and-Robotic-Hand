579e5f383c2d36fb67bfcdabdf7b80c3b21d748d
SHiTU23 <shi.tu2323@gmail.com>
2024-06-27 18:44:12 +0330

    add comments to code
    + where record gets started and ended

--- src/gaze_tracker/pupile_remote.py
+++ src/gaze_tracker/pupile_remote.py
@@ -6,9 +6,11 @@ pupil_remote = zmq.Socket(context, zmq.REQ)
 ### Local Host and Default IP address for Pupil Core Software
 pupil_remote.connect('tcp://127.0.0.1:50020')
 
+### Start recording
 pupil_remote.send_string('R')
 print(pupil_remote.recv_string())
 
 sleep(5)
+### Stop recording
 pupil_remote.send_string('r')
 print(pupil_remote.recv_string())
